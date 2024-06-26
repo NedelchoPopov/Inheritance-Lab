fcbaa510bb3dd8111d839fceade2e719d1f83aa6
Nedelcho <160885648+NedelchoPopov@users.noreply.github.com>
2024-06-26 20:44:43 +0300

    Add project files.

---
+++ CustomRandomList/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+             Random random = new (Count);
+            int index = random.Next();
+            string element = this[index];
+            RemoveAt (index);
+            return element;
+        }
+    }
+}
---
+++ CustomStack/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new List<string> { "ivan", "deni"});
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
